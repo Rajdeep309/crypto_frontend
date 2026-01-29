cd9d72fb03451184600477d51708b385736b853e
Rajdeep309 <rajdeepverma80@gmail.com>
2026-01-29 22:56:37 +0530

    Update API base URL to use HTTPS

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://cryptoportfoliotrackerbackend1-production.up.railway.app",
+  baseURL: "https://cryptoportfoliotrackerbackend1-production.up.railway.app",
 });
 
 api.interceptors.request.use(
